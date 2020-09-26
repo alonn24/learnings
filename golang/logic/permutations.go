fd3f4ecb77c0a2631cb8d0aa8c2b83eafda07aa1
Alon Yehezkel <alony@bookaway.com>
2020-09-26 15:27:15 +0300

    add better array manipulation

--- golang/logic/permutations.go
+++ golang/logic/permutations.go
@@ -1,23 +1,42 @@
 package permutations
 
+import "log"
+
 // rune - an alias for the type int32
 
 // Perm - create pemutations
-func Perm(input string) (result [][]rune) {
-	r := []rune(input)
-	result = perm(r, 0)
+func Perm(input string) (result []string) {
+	result = perm(input)
 	return
 }
 
-func perm(a []rune, i int) (res [][]rune) {
-	if i > len(a) {
-		return append(res, a)
+func removeAtIndex(input string, i int) (selected rune, str string) {
+	a := []rune(input)
+	selected = a[i]
+	rest := append([]rune{}, a[:i]...)
+	rest = append(rest, a[i+1:]...)
+	str = string(rest)
+	return
+}
+
+func appendToStart(l rune, input string) string {
+	x := append([]rune{l}, []rune(input)...) // convert string to rune
+	return string(x)
+}
+
+func perm(input string) (res []string) {
+	log.Printf("perm: %v\n", input)
+	if len(input) == 1 {
+		res = append(res, input)
+	} else {
+		for i := 0; i < len(input); i++ {
+			selected, rest := removeAtIndex(input, i)
+			for _, p := range perm(rest) {
+				res = append(res, appendToStart(selected, p))
+			}
+		}
 	}
-	res = append(res, perm(a, i+1)...)
-	for j := i + 1; j < len(a); j++ {
-		a[i], a[j] = a[j], a[i]
-		res = append(res, perm(a, i+1)...)
-		a[i], a[j] = a[j], a[i]
-	}
+
+	log.Printf("perm return : %v\n", res)
 	return
 }
