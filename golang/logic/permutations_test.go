d819633d7accdea7ea6b8a59fa7f3481aaf32c41
Alon Yehezkel <alony@bookaway.com>
2020-09-26 15:45:45 +0300

    better tests

--- golang/logic/permutations_test.go
+++ golang/logic/permutations_test.go
@@ -1,12 +1,20 @@
 package permutations
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestMain(m *testing.M) {
+	log.SetOutput(ioutil.Discard)
+	os.Exit(m.Run())
+}
 
 func TestPerm(t *testing.T) {
 	ans := Perm("abc")
-	if len(ans) != 6 {
-		t.Fatalf("Perm(\"abc\") = %v, wanted 6 results", ans)
-	}
+	assert.Len(t, ans, 6)
 }
