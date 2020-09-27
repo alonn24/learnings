ae808c62d4136f1725af62f783993bd9ac3034ca
Alon Yehezkel <alony@bookaway.com>
2020-09-27 11:16:05 +0300

    Refactor get next rolls

--- golang/bowling-game-kata/game.go
+++ golang/bowling-game-kata/game.go
@@ -15,29 +15,23 @@ func (game *Game) roll(score int) *Game {
 	return game
 }
 
-func getFrameAt(game Game, i int) *frame {
+func getRollsForFrame(game Game, i int) []int {
 	if len(game.frames) > i {
-		return &game.frames[i]
+		return game.frames[i].rolls
 	}
-	return nil
+	return []int{}
 }
 
-func getNextRolls(game Game, i int) (int, int) {
-	nextFrame := getFrameAt(game, i+1)
-	next2Frame := getFrameAt(game, i+2)
-	if nextFrame == nil {
-		return 0, 0
+func getNextTwoRolls(game Game, i int) (int, int) {
+	nextRolls := append(getRollsForFrame(game, i+1), getRollsForFrame(game, i+2)...)
+	r1 := 0
+	if len(nextRolls) > 0 {
+		r1 = nextRolls[0]
 	}
 
-	r1 := 0
 	r2 := 0
-	if len(nextFrame.rolls) >= 1 {
-		r1 = nextFrame.rolls[0]
-	}
-	if nextFrame.isStrike() && next2Frame != nil && len(next2Frame.rolls) >= 1 {
-		r2 = next2Frame.rolls[0]
-	} else if len(nextFrame.rolls) >= 2 {
-		r2 = nextFrame.rolls[1]
+	if len(nextRolls) > 1 {
+		r2 = nextRolls[1]
 	}
 	return r1, r2
 }
@@ -47,7 +41,7 @@ func (game Game) score() int {
 	total := 0
 	for i := 0; i < len(game.frames); i++ {
 		frame := game.frames[i]
-		nr1, nr2 := getNextRolls(game, i)
+		nr1, nr2 := getNextTwoRolls(game, i)
 		total += frame.getScore()
 		if frame.isStrike() {
 			total += nr1 + nr2
