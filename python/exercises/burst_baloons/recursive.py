b3473936db5afde2be459921347349ac2815ed2e
Alon Yehezkel <alony@bookaway.com>
2021-06-22 09:55:11 +0300

    Python baloon burst dp solution

--- python/exercises/burst_baloons/recursive.py
+++ python/exercises/burst_baloons/recursive.py
@@ -2,21 +2,43 @@ import functools
 from typing import List
 
 
+def baloonBurstRecursive(nums: List[int]) -> int:
+    nums = [1] + nums + [1]
+
+    @functools.cache
+    # i,j are indexses of first and last
+    def recursive(i, j):
+        if (i >= j):
+            return 0
+        result = 0
+        for k in range(i+1, j):  # j is inclusive
+            left = recursive(i, k)
+            right = recursive(k, j)
+            result = max(result, left + nums[i]*nums[k]*nums[j] + right)
+        return result
+    return recursive(0, len(nums) - 1)
+
+
+def baloonBurstDP(nums: List[int]) -> int:
+    nums = [1] + nums + [1]
+    n = len(nums)
+    dp = [[0] * n for _ in range(n)]
+
+    def add_to_dp(i, j):
+        result = 0
+        for k in range(i+1, j):
+            left = dp[i][k]
+            right = dp[k][j]
+            result = max(result, left + nums[i]*nums[k]*nums[j] + right)
+        print(i, j, '->', result)
+        dp[i][j] = result
+
+    for i in range(2, n):
+        for j in range(0, n-i):
+            add_to_dp(j, j+i)
+    return dp[0][n-1]
+
+
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        nums = [1] + nums + [1]
-
-        @functools.cache
-        def baloonBurstRecursive(i: int, j: int) -> int:
-            # n = len(nums)
-            if (i >= j):
-                return 0
-            result = 0
-            for k in range(i+1, j):
-                left = baloonBurstRecursive(i, k)
-                right = baloonBurstRecursive(k, j)
-                current = nums[i]*nums[k]*nums[j]
-                result = max(result, left + right + current)
-            return result
-
-        return baloonBurstRecursive(0, len(nums) - 1)
+        return baloonBurstDP(nums)
--- python/exercises/burst_baloons/recursive_test.py
+++ python/exercises/burst_baloons/recursive_test.py
@@ -3,15 +3,15 @@ solution = Solution()
 
 
 def test_recursion():
-    assert solution.maxCoins([]) == 0
+    # assert solution.maxCoins([]) == 0
     assert solution.maxCoins([1]) == 1
-    assert solution.maxCoins([1, 5]) == 10
-    assert solution.maxCoins([5]) == 5
-    assert solution.maxCoins([3, 5, 2]) == 39
-    assert solution.maxCoins([3, 1, 5, 8]) == 167
+    # assert solution.maxCoins([1, 5]) == 10
+    # assert solution.maxCoins([5]) == 5
+    # assert solution.maxCoins([3, 5, 2]) == 39
+    # assert solution.maxCoins([3, 1, 5, 8]) == 167
 
 
-def test_timeLimmit():
-    assert solution.maxCoins(
-        [2, 4, 8, 4, 0, 7, 8, 9, 1, 2, 4, 7, 1, 7, 3, 6]) == 3304
+# def test_timeLimmit():
+#     assert solution.maxCoins(
+#         [2, 4, 8, 4, 0, 7, 8, 9, 1, 2, 4, 7, 1, 7, 3, 6]) == 3304
     # assert solution.maxCoins([x for x in range(1000)]) == 10
